6668782781c50234490bffd6b64be69f1f91cb1d
Johanna Hultén <min.mail@johannap.se>
2024-11-03 11:14:50 +0100

    Updates

--- WhatDoWeEat.App/src/app/app.component.ts
+++ WhatDoWeEat.App/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatModules } from './shared/mat-modules';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ...MatModules],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
