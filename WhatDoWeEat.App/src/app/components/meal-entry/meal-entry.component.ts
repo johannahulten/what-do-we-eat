ad92dc2c2eabc706471baed5b3fffa937d29a660
Johanna Hultén <min.mail@johannap.se>
2024-11-03 10:45:32 +0100

    Setup for project for angular

---
+++ WhatDoWeEat.App/src/app/components/meal-entry/meal-entry.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meal-entry',
+  templateUrl: './meal-entry.component.html',
+  styleUrls: ['./meal-entry.component.scss']
+})
+export class MealEntryComponent {
+  // Will contain logic for adding/editing meals
+} 
---
+++ WhatDoWeEat.App/src/app/components/print-view/print-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-print-view',
+  templateUrl: './print-view.component.html',
+  styleUrls: ['./print-view.component.scss']
+})
+export class PrintViewComponent {
+  // Will contain logic for print layout
+} 
